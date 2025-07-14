da7556f627d19e1f72d3165f52ab16e108c57c5d
Kyan Barker <94871674+kyanbarker@users.noreply.github.com>
2025-07-14 16:30:46 -0700

    Removed deprecated files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
 import { Box } from "@mui/material";
-import { differenceInDays, max } from "date-fns";
 import { useState } from "react";
-import EditableTable from "./components/EditableTable";
-import HighlightedCalendar from "./components/HighlightedCalendar";
 import ClickController from "./components/ClickController";
+import HighlightedCalendar from "./components/HighlightedCalendar";
 
 function App() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
@@ -16,18 +14,6 @@ function App() {
     new Date(2025, 3, 18),
     new Date(2025, 3, 20),
   ];
-  const lastEngagement = max(engagementDates);
-  const numDaysSinceLastEngagement = differenceInDays(
-    new Date(),
-    lastEngagement
-  );
-
-  const [rows, setRows] = useState([
-    ["habit", "value", "unit", "notes"],
-    ["reading", "10", "pages", ""],
-    ["mood", "7", "out of 10", ""],
-    ["exercise", "30", "minutes", "running"],
-  ]);
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2, padding: 2 }}>
@@ -43,22 +29,6 @@ function App() {
           disableFuture={true}
         />
       </ClickController>
-      <h2>{selectedDate?.toDateString() || "No date selected"}</h2>
-      <h2>
-        Last Engagement: {lastEngagement.toDateString()} (
-        {numDaysSinceLastEngagement} Days Ago)
-      </h2>
-      <Box sx={{ height: "400px", width: "800px", overflow: "auto" }}>
-        <EditableTable
-          rows={rows}
-          setRows={setRows}
-          columnConfig={[
-            { isLocked: true },
-            { type: "number" },
-            { isLocked: true },
-          ]}
-        />
-      </Box>
     </Box>
   );
 }
